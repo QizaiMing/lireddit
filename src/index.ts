5420ab5c8135070f917e2a396c6947a50d21c8fe
Juan Chacon <mahnimex@gmail.com>
2021-01-22 17:10:45 -0400

    Added DataLoader

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,8 @@ import { User } from './entities/User'
 import { secrets } from './utils/secrets'
 import path from 'path'
 import { Updoot } from './entities/Updoot'
+import { createUserLoader } from './utils/createUserLoader'
+import { createUpdootLoader } from './utils/createUpdootLoader'
 
 const main = async () => {
   const conn = await createConnection({
@@ -69,7 +71,13 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false
     }),
-    context: ({ req, res }): MyContext => ({ req, res, redis })
+    context: ({ req, res }): MyContext => ({
+      req,
+      res,
+      redis,
+      userLoader: createUserLoader(),
+      updootLoader: createUpdootLoader()
+    })
   })
 
   apolloServer.applyMiddleware({
