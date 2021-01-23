e3a47b59f9a8b5f5f061d417436154034814bf6c
Juan Chacon <mahnimex@gmail.com>
2021-01-23 15:32:08 -0400

    ready for deploy

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata'
+import 'dotenv-safe/config'
 import { COOKIE_NAME, __prod__ } from './constants'
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
@@ -14,7 +15,6 @@ import cors from 'cors'
 import { createConnection } from 'typeorm'
 import { Post } from './entities/Post'
 import { User } from './entities/User'
-import { secrets } from './utils/secrets'
 import path from 'path'
 import { Updoot } from './entities/Updoot'
 import { createUserLoader } from './utils/createUserLoader'
@@ -23,26 +23,22 @@ import { createUpdootLoader } from './utils/createUpdootLoader'
 const main = async () => {
   const conn = await createConnection({
     type: 'postgres',
-    database: secrets.dbName,
-    username: secrets.dbUser,
-    password: secrets.dbPassword,
+    url: process.env.DATABASE_URL,
     logging: true,
-    synchronize: true,
+    // synchronize: true,
     migrations: [path.join(__dirname, './migrations/*')],
     entities: [Post, User, Updoot]
   })
   await conn.runMigrations()
 
-  // await Post.delete({})
-
   const app = express()
 
   const RedisStore = connectRedis(session)
-  const redis = new Redis()
-
+  const redis = new Redis(process.env.REDIS_URL)
+  app.set('proxy', 1)
   app.use(
     cors({
-      origin: 'http://localhost:3000',
+      origin: process.env.CORS_ORIGIN,
       credentials: true
     })
   )
@@ -61,7 +57,7 @@ const main = async () => {
         secure: __prod__ // cookie only works in https
       },
       saveUninitialized: false,
-      secret: 'oqweuryiouqyeriuefhwhfsdweiorywi',
+      secret: process.env.SESSION_SECRET,
       resave: false
     })
   )
@@ -85,7 +81,7 @@ const main = async () => {
     cors: { origin: false }
   })
 
-  app.listen(4000, () => {
+  app.listen(parseInt(process.env.PORT), () => {
     console.log('server started on localhost:4000')
   })
 }
